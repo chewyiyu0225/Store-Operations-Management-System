d63d30b8140012825837d06f0931f2e6f25bffbb
Tan Li Hong-2025 <100345311+lihongc457gmailco@users.noreply.github.com>
2025-12-23 22:23:18 +0800

    Refactor Employee class for improved readability

--- Employee.java
+++ Employee.java
@@ -2,30 +2,30 @@ package fop;
 
 public class Employee {
     // Fields matching the CSV columns: ID, Name, Password, Role
-    String inputemployeeID,inputname,inputpassword,inputrole;
+    String employeeID,name,password,role;
 
     public Employee(String employeeID, String name, String password, String role) {
-        inputemployeeID = employeeID;
-        inputname = name;
-        inputpassword = password;
-        inputrole = role;
+        this.employeeID = employeeID;
+        this.name = name;
+        this.password = password;
+        this.role = role;
     }
 
-    public String getEmployeeID() { return inputemployeeID; }
-    public String getName() { return inputname; }
-    public String getPassword() { return inputpassword; }
-    public String getRole() { return inputrole; }
+    public String getEmployeeID() { return this.employeeID; }
+    public String getName() { return this.name; }
+    public String getPassword() { return this.password; }
+    public String getRole() { return this.role; }
 
-    public void setName(String name) { inputname = name; }
-    public void setPassword(String password) { inputpassword = password; }
-    public void setRole(String role) { inputrole = role; }
+    public void setName(String name) { this.name = name; }
+    public void setPassword(String password) { this.password = password; }
+    public void setRole(String role) { this.role = role; }
 
     public String toCSV() {
-        return inputemployeeID + "," + inputname + "," + inputpassword + "," + inputrole;
+        return employeeID + "," + name + "," + password + "," + role;
     }
 
     public String toString() {
-        return "ID: " + inputemployeeID + " | Name: " + inputname + " (" + inputrole + ")";
+        return "ID: " + employeeID + " | Name: " + name + " (" + role + ")";
     }
 }
 
